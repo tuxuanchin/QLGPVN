50e45e9845d7aaa1c09ae3fd8428d4ceee8fe3bc
thainguyen1912 <>
2019-09-20 11:00:09 +0700

    a

---
+++ project/Model/DBconfig.php
@@ -0,0 +1,10 @@
+<?php
+    class Database{
+        private $hostname='localhost:3306';
+        private $username='root';
+        private $pass='2411';
+        private $dbname='quanlygiapha';
+    }
+
+
+?>
