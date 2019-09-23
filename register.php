d4830da069de17ee058ae717262dfa7cdffb0293
tuxuanchin <32194978+tuxuanchin@users.noreply.github.com>
2019-09-23 17:06:27 +0700

    capnhat

--- register.php
+++ register.php
@@ -32,21 +32,21 @@
 			<div class="wrap-login100">
 				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178">
 					<span class="login100-form-title">
-						Sign Up
+						Đăng ký
 					</span>
 
-					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
-						<input class="input100" type="text" name="username" placeholder="Username">
+					<div class="wrap-input100 validate-input m-b-16" data-validate="Nhập tên tài khoản">
+						<input class="input100" type="text" name="username" placeholder="Tên tài khoản">
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input m-b-16" data-validate = "Please enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+					<div class="wrap-input100 validate-input m-b-16" data-validate = "Nhập mật khẩu">
+						<input class="input100" type="password" name="pass" placeholder="Nhập mật khẩu">
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Please enter password">
-						<input class="input100" type="password" name="pass" placeholder="Retype Password">
+					<div class="wrap-input100 validate-input" data-validate = "Nhập lại mật khẩu">
+						<input class="input100" type="password" name="pass" placeholder="Nhập lại mật khẩu">
 						<span class="focus-input100"></span>
 					</div>
 
@@ -56,25 +56,11 @@
 
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
-							Sign up
+							Đăng ký
 						</button>
 					</div>
 
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn" onclick="location.href='../page/index_trangchu.html';">
-							Cancel
-						</button>
-					</div>
-
-					<div class="flex-col-c p-t-170 p-b-40">
-						<span class="txt1 p-b-9">
-							Don’t have an account?
-						</span>
-
-						<a href="Sign in.html" class="txt3">
-							Sign in now
-						</a>
-					</div>
+					
 				</form>
 			</div>
 		</div>
